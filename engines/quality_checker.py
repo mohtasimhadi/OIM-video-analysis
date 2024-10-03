9642cc3e3fe24813921ff5e4dfb87acb457ca0a2
Mohtasim Hadi Rafi <mzr0167@auburn.edu>
2024-10-03 15:31:19 -0500

    quality assessment

---
+++ engines/quality_checker.py
@@ -0,0 +1,29 @@
+import numpy as np
+import cv2
+import math
+
+def quality_assessment(mask):
+    contour = np.array(mask, dtype=np.int32).reshape((-1, 1, 2))
+    area = cv2.contourArea(contour)
+    perimeter = cv2.arcLength(contour, closed=True)
+
+    if perimeter > 0:
+        circularity = (4 * math.pi * area) / (perimeter ** 2)
+    else:
+        circularity = 0
+
+    if len(contour) >= 5:  # Minimum number of points required to fit an ellipse
+        ellipse = cv2.fitEllipse(contour)
+        (center, axes, orientation) = ellipse
+        major_axis = max(axes)
+        minor_axis = min(axes)
+        eccentricity = np.sqrt(1 - (minor_axis / major_axis) ** 2)
+    else:
+        eccentricity = 0
+
+    return {
+        'area': float(area),
+        'perimeter': float(perimeter),
+        'circularity': float(circularity),
+        'eccentricity': float(eccentricity)
+    }
