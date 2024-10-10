56bc59537d81f621aabf8fff66b873bda1be79c1
Mohtasim Hadi Rafi <mzr0167@auburn.edu>
2024-10-10 18:04:53 -0500

    uri fix

--- utils/config.py
+++ utils/config.py
@@ -1,5 +1,5 @@
 class Config:
     CDN_URI   = "http://localhost:8000"
     GET_VIDEO = "/video/view/"
-    PUT_VIDEO = "/video/upload"
-    PUT_IMAGE = "/image/upload"
+    PUT_VIDEO = "/video/upload/"
+    PUT_IMAGE = "/image/upload/"
